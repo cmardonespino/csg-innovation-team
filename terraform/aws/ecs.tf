f1ce7da953bba86f59091612c6865d4e716e2a23
carlos <carlos.mardones@vacasa.com>
2024-05-15 19:51:29 -0400

    Input changes due module have changes and new features

--- terraform/aws/ecs.tf
+++ terraform/aws/ecs.tf
@@ -1,10 +1,12 @@
 module "ecs_service" {
   source       = "../../modules/aws_ecs_service/"
   cluster_name = "csg-innovation-team"
+  lb_internal = false
   service_name    = "csg-innovation-service"
-  vpc_id = data.aws_vpc.vpc.id
-  vpc_cidr_block = data.aws_vpc.vpc.cidr_block
-  subnet_ids = data.aws_subnet_ids.subnets.ids
+  vpc_id = aws_vpc.vpc.id
+  vpc_cidr_block = aws_vpc.vpc.cidr_block
+  ecs_subnet_ids = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+  alb_subnet_ids = [aws_subnet.public[0].id, aws_subnet.public[1].id]
   container_definition = data.template_file.container_definition.rendered
 }
 
