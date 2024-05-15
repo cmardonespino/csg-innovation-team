37150633955c1c55211d1d47b60f9611d8641227
carlos <carlos.mardones@vacasa.com>
2024-05-14 21:50:44 -0400

    Add IAM Role for ECS service

---
+++ modules/aws_ecs_service/iam.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_role" "ecs_role" {
+  name = "${var.service_name}-ecs-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    "Name" = "${var.service_name}-ecs-role"
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_role_policy" {
+  name = "${var.service_name}-ecs-policy-role"
+  role = aws_iam_role.ecs_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+}
